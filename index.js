fc882f946dd38df00a27d5ca39cf65f0aebd1909
Urvish Prajapati <urvishprajapati412@gmail.com>
2023-10-30 16:08:21 -0400

    oop-transaction added

--- index.js
+++ index.js
@@ -1,29 +1,75 @@
 let balance = 500.00;
 
-class Withdrawal {
+class Transaction {
 
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    // Keep track of the time of the transaction
+    this.time = new Date();
+    // Add the transaction to the account
+    this.account.addTransaction(this);
+  }
+
+}
+class Withdrawal extends Transaction {
+
+  value() {
+    return this.amount;
+  }
+
+  commit() {
+    this.account.balance -= this.value();
   }
 
 }
 
+class Deposit extends Transaction {
 
+  value() {
+    return this.amount;
+  }
 
+  commit() {
+    this.account.balance += this.value();
+  }
 
+}
+
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
+  }
+
+  getBalance() {
+    return this.balance;  // Calculate the balance using the transaction objects.
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+
+}
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25);
+let t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+let t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+let t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+console.log('Balance:', myAccount.balance);
